27127aed64ebdc89e6392e22af80de2d29f59085
samedamci <samedamci@disroot.org>
2020-05-08 22:30:13 +0200

    Restore Arabic numbers back on statusbar

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
