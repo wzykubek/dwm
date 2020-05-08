7a5ddf07468212b12c29721f5aed80dd8e45ec9b
samedamci <samedamci@disroot.org>
2020-05-08 22:03:59 +0200

    Change font size from 10 to 9

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]     = "#1b1b1b";
 static char normbordercolor[] = "#444444";
